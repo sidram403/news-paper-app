8dd5011a460dc21079e62aade021d68deb6b9536
Amit Gharge <amitgharge99@gmail.com>
2024-06-09 12:40:54 +0530

    update code for user update

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -31,22 +31,19 @@ export const updateUser = async (req, res, next) => {
   };
 
   export const getUserListings = async (req, res, next) => {
-    if (req.user.id === req.params.id) {
+    // if (req.user.id === req.params.id) {
       try {
         const listings = await Listing.find({ userRef: req.params.id });
         res.status(200).json(listings);
       } catch (error) {
         next(error);
       }
-    } else {
-      return next(errorHandler(401, 'You can only view your own listings!'));
-    }
+    
   };
   
   
   export const deleteUser = async (req, res, next) => {
-    if (req.user.id !== req.params.id)
-      return next(errorHandler(401, 'You can only delete your own account!'));
+    
     try {
       await User.findByIdAndDelete(req.params.id);
       res.clearCookie('access_token');
