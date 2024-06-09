f01c7e74e413b35be7797e3efd9b161acf31409d
Amit Gharge <amitgharge99@gmail.com>
2024-06-09 12:32:11 +0530

    update code for user update

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,8 +2,8 @@ import Listing from "../models/listing.model.js";
 import User from "../models/user.model.js";
 
 export const updateUser = async (req, res, next) => {
-    if (req.user.id !== req.params.id)
-      return next(errorHandler(401, 'You can only update your own account!'));
+    // if (req.user.id !== req.params.id)
+    //   return next(errorHandler(401, 'You can only update your own account!'));
     try {
       if (req.body.password) {
         req.body.password = bcryptjs.hashSync(req.body.password, 10);
